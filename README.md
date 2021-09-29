ad4853fbec6bbfd9d3935952abee4f4c310e8bb0
Asher Mancinelli <ashermancinelli@gmail.com>
2021-09-29 14:35:26 -0600

    Adding sanity check driver

---
+++ README.md
@@ -0,0 +1,7 @@
+## RAJA and BQN examples
+
+Write GPU-portable implementations of BQN solutions.
+
+## Installation
+
+See [Umpire](https://github.com/LLNL/Umpire) and [RAJA](https://github.com/LLNL/RAJA) repos for installation instructions.
---
+++ src/dependency-check.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#include <RAJA/RAJA.hpp>
+#include <cuda.h>
+#include <umpire/Allocator.hpp>
+#include <umpire/ResourceManager.hpp>
+
+using RAJA::forall;
+using RAJA::RangeSegment;
+
+int main(int, char **) {
+
+  static constexpr std::size_t N = 100;
+  using exec_space = RAJA::cuda_exec<128>;
+
+  auto &rm = umpire::ResourceManager::getInstance();
+  umpire::Allocator alloc = rm.getAllocator("UVM");
+
+  auto *arr = static_cast<double *>(alloc.allocate(N * sizeof(double)));
+
+  forall<exec_space>(
+      RangeSegment(0, N), __device__[=](const int i) { arr[i] = 1.; });
+
+  std::cout << "Running dependency sanity check.\n";
+  return 0;
+}
