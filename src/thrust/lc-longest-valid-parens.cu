f7f1332c89b7979bcb653ee274e8a162d61b4e6d
Asher Mancinelli <ashermancinelli@gmail.com>
2021-10-11 16:21:22 -0600

    Cleaned up lc longest valid parens solution

--- src/thrust/lc-longest-valid-parens.cu
+++ src/thrust/lc-longest-valid-parens.cu
@@ -1,35 +1,32 @@
 #include <thrust-config.hpp>
 
-auto solve(string problem) -> void {
+auto solve(const string& problem) -> int {
   const int N = problem.size();
+  if (0 == N)
+    return 0;
+
   host_vector<int> mapping;
   mapping.reserve(N);
   std::transform(problem.begin(), problem.end(), std::back_inserter(mapping),
                  [=](const char &c) { return c == '(' ? 1 : -1; });
   device_vector<int> d_mapping = mapping;
 
-  // Copy prefix from the input to the prefix array
-  device_vector<int> prefixes(N * N);
   vector<int> starts(N - 1);
   std::iota(starts.begin(), starts.end(), 0);
+
   int max_len = std::accumulate(
       starts.begin(), starts.end(), 0,
-      [&prefixes, &d_mapping, N](int max_so_far, int i) {
-        thrust::copy(d_mapping.begin() + i, d_mapping.end(),
-                     prefixes.begin() + i * N);
-        auto prefix_start = prefixes.begin() + i * N;
-        auto prefix_end = prefixes.begin() + (i * N) + (N - i);
-
-        thrust::inclusive_scan(thrust::device, prefix_start, prefix_end,
-                               prefix_start);
+      [&d_mapping, N](int max_so_far, int i) {
+        device_vector<int> prefix(N-i);
+        thrust::inclusive_scan(d_mapping.begin()+i, d_mapping.end(), prefix.begin());
 
         device_vector<int> indices(N - i);
         thrust::sequence(indices.begin(), indices.end(), 0);
 
         auto zip_start = thrust::make_zip_iterator(
-            thrust::make_tuple(prefix_start, indices.begin()));
+            thrust::make_tuple(prefix.begin(), indices.begin()));
         auto zip_end = thrust::make_zip_iterator(
-            thrust::make_tuple(prefix_end, indices.end()));
+            thrust::make_tuple(prefix.end(), indices.end()));
 
         int max_for_prefix = thrust::transform_reduce(
             zip_start, zip_end,
@@ -41,15 +38,11 @@ auto solve(string problem) -> void {
         return std::max(max_so_far, max_for_prefix);
       });
 
-  std::cout << "Max = " << max_len << "\n";
+  return max_len;
 }
 
 int main() {
-  vector<string> input{
-      ")()())",
-      "(()",
-  };
-  for (const auto &problem : input)
-    solve(problem);
+  for (const string &problem : { ")()())", "(()", "" })
+    std::cout << solve(problem) << "\n";
   return 0;
 }
