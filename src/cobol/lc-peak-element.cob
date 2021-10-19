960ed6cb7dcda852da8eb7fd4b3f33644946f640
Asher Mancinelli <ashermancinelli@gmail.com>
2021-10-19 12:43:32 -0600

    stub for cobol solution

---
+++ src/cobol/lc-peak-element.cob
@@ -0,0 +1,10 @@
+            *> setup the identification division
+            IDENTIFICATION DIVISION.
+            *> setup the program id
+            PROGRAM-ID. LCPEAKELEMENT.
+            *> setup the procedure division (like 'main' function)
+            PROCEDURE DIVISION.
+              *> print a string
+              DISPLAY 'Cobol compiler is working'.
+            *> end our program
+            STOP RUN.
